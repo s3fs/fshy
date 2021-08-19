6cd35811ae86b49f56e1e46f9376545c6ac8bab9
s3fs <northfacebodybags@yahoo.com>
2021-08-19 23:11:54 +0500

    loginform refactor

---
+++ src/components/login.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const LoginForm = ({
+    handleSubmit,
+    handlePasswordChange,
+    handleUsernameChange,
+    username,
+    password
+}) => {
+    return (
+        <form onSubmit={handleSubmit}>
+        <div>
+          username 
+          <input
+            type='text'
+            name='Username'
+            alt='poopie'
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <div>
+          passsword 
+          <input
+            type='text'
+            name='Password'
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type='submit'>Login</button>
+      </form>
+    )
+}
+
+export default LoginForm
