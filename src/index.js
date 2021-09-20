b781e68cd3991efd7b30a0f08bc89cca97faf381
s3fs <northfacebodybags@yahoo.com>
2021-09-20 13:21:46 +0500

    router init

--- src/index.js
+++ src/index.js
@@ -1,5 +1,64 @@
+import React from 'react-dom'
 import ReactDOM from 'react-dom'
-import App from './App.js'
-import './index.css'
+import {
+  BrowserRouter as Router,
+  Switch, Route, Link
+} from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <div>
+      <h2>TLDR Notes App</h2>
+    </div>
+  )
+}
+
+const Notes = () => {
+  return (
+    <div>
+      <h2>Notes</h2>
+    </div>
+  )
+}
+
+const Users = () => {
+  return (
+    <div>
+      <h2>Users</h2>
+    </div>
+  )
+}
+
+const App = () => {
+  const padding = {
+    padding: 5
+  }
+
+  return (
+    <Router>
+      <div>
+        <Link style={padding} to='/'>home</Link>
+        <Link style={padding} to='/notes'>notes</Link>
+        <Link style={padding} to='/users'>users</Link>
+      </div>
+      <Switch>
+        <Route path='/notes'>
+          <Notes />
+        </Route>
+        <Route path='/users'>
+          <Users />
+        </Route>
+        <Route path='/'>
+          {/** slash-root path the last one cuz otherwise switch loop terminates on it */}
+          <Home />
+        </Route>
+      </Switch>
+
+      <div>
+        <i>Note App Property of me</i>
+      </div>
+    </Router>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
