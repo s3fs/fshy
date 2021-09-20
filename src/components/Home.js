0aff79f747411f895d469de7bd1c651533551734
s3fs <northfacebodybags@yahoo.com>
2021-09-20 11:25:23 +0500

    m

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Notes app</h2>
+      <p>
+        Notes app for notes. Note, notes, app. Notes. 
+        Gfvsdmkzvc hnfudncmk ndeiowm dl noina opi. 
+        Fdcns iorjg mijo vbuhzc. 
+        Xvnduievcnnoivcn uieo nopkmhnpohm.
+      </p>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Users.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Users = () => {
+  return (
+    <div>
+      <p>future users page</p>
+    </div>
+  )
+}
+
+export default Users
